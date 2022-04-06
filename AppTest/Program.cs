19d44769e854cfb91282633a6d6cfa54b57c5af6
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-04-06 18:11:58 +0200

    Ajout du for et foreach

--- AppTest/Program.cs
+++ AppTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AppTest
 {
@@ -6,17 +8,47 @@ namespace AppTest
     {
         static void Main(string[] args)
         {
+            //TODO : Faire une application pour calculer une moyenne
 
-            bool condition = false;
+            Console.WriteLine("Bienvenue dans votre application de calcul de moyenne.");
 
-            do
+            Console.WriteLine("Tapez vos chiffres, puis entrée. Lorsque c'est bon, tapez OK");
+
+            List<int> mesNombres = new List<int>();
+
+            bool continueExecution = true;
+
+            while (continueExecution)
             {
-                Console.WriteLine("Condition est fausse");
-            }
-            while (condition);
+                string nombre = Console.ReadLine();
 
+                try
+                {
+                    int nombreEntier = Convert.ToInt32(nombre);
+                    mesNombres.Add(nombreEntier);
+                }
+                catch(Exception ex)
+                {
+                    continueExecution = false;
+                }
+            }
+
+            //Calcul de la moyenne
+
+            int total = 0;
+            foreach (int nombreCourant in mesNombres)
+            {
+                total = total + nombreCourant;
+            }
+
+
+            // int              double  /  int     => double
+            var moyenne = (double)total / mesNombres.Count;
+            moyenne = Math.Round(moyenne, 2);
+            Console.WriteLine("Moyenne : "+ moyenne);
 
             Console.ReadLine();
+
         }
     }
 }
