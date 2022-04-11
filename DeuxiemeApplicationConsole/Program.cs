27aa6cd2e8f414ddf68d4793e68cd12333463228
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-04-11 12:33:13 +0200

    Ajout des méthodes

--- DeuxiemeApplicationConsole/Program.cs
+++ DeuxiemeApplicationConsole/Program.cs
@@ -12,68 +12,7 @@ namespace Calculatrice
             bool continueExecution;
             do
             {
-                #region iterationDeCalcul
-                Console.WriteLine("Début du calcul !");
-
-                //Récupère le calcul auprès de l'utilisateur
-
-                Console.WriteLine("Premier nombre : ");
-                string premierChiffre = Console.ReadLine();
-
-                Console.WriteLine("Opération (+ - / *)");
-                string operation = Console.ReadLine();
-
-                Console.WriteLine("Deuxième nombre");
-                string deuxiemeChiffre = Console.ReadLine();
-
-                //J'ai tout ce qu'il me faut dans mes variables
-                //Faire le calcul
-                try
-                {
-                    int monPremierVraiChiffre = Convert.ToInt32(premierChiffre);
-                    int monDeuxiemeVraiChiffre = Convert.ToInt32(deuxiemeChiffre);
-
-                    //En fonction de la valeur de 'operation'
-                    //Effectuer la bonne opération
-                    double resultat = 0;
-                    bool operateurOK = true;
-                    switch (operation)
-                    {
-                        case "+":
-                            resultat = monPremierVraiChiffre + monDeuxiemeVraiChiffre;
-                            break;
-                        case "-":
-                            resultat = monPremierVraiChiffre - monDeuxiemeVraiChiffre;
-                            break;
-                        case "*":
-                        case "x":
-                        case "X":
-                            resultat = monPremierVraiChiffre * monDeuxiemeVraiChiffre;
-                            break;
-                        case "/":
-                            resultat = monPremierVraiChiffre / monDeuxiemeVraiChiffre;
-                            break;
-                        default:
-                            operateurOK = false;
-                            break;
-                    }
-
-                    if (operateurOK)
-                    {
-                        Console.WriteLine("Résultat : " + resultat);
-                    }
-                    else
-                    {
-                        Console.WriteLine("L'opérateur n'est pas bon");
-                    }
-
-                    Console.WriteLine("");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("le nombre n'est pas bon");
-                }
-                #endregion
+                doCalculComplet();
 
                 //Savoir si l'utilisateur veut faire un autre calcul
                 Console.WriteLine("Voulez-vous faire un autre calcul (O/N)?");
@@ -88,6 +27,83 @@ namespace Calculatrice
                 }
             }
             while (continueExecution);
+        }
+
+        static void doCalculComplet()
+        {
+            Console.WriteLine("Début du calcul !");
+
+            //Récupère le calcul auprès de l'utilisateur
+
+            Console.WriteLine("Premier nombre : ");
+            string premierChiffre = Console.ReadLine();
+
+            Console.WriteLine("Opération (+ - / *)");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Deuxième nombre");
+            string deuxiemeChiffre = Console.ReadLine();
+
+            //J'ai tout ce qu'il me faut dans mes variables
+            //Faire le calcul
+            try
+            {
+                int monPremierVraiChiffre = Convert.ToInt32(premierChiffre);
+                int monDeuxiemeVraiChiffre = Convert.ToInt32(deuxiemeChiffre);
+
+                var resultat = Calcul(monPremierVraiChiffre, operation, monDeuxiemeVraiChiffre);
+
+                if (resultat != null)
+                {
+                    Console.WriteLine("Résultat : " + resultat);
+                }
+                else
+                {
+                    Console.WriteLine("L'opérateur n'est pas bon");
+                }
+
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("le nombre n'est pas bon");
+            }
+        }
+
+        /// <summary>
+        /// Retourne le résultat du calcul en fonction des nombres et de l'opérateur. Si l'opérateur n'est pas bon, retourne null
+        /// Une divizion par 0 retourne l'exception ErrorDivideBy0
+        /// </summary>
+        /// <param name="nombre1">Premier nombre du calcul</param>
+        /// <param name="operateur">Operateur de calcul (+,-,*,x,X,/)</param>
+        /// <param name="nombre2">Deuxième nombre du calcul</param>
+        /// <returns>Si opérateur ok -> résultat calcul sinon null</returns>
+        static double? Calcul(int nombre1, string operateur, int nombre2)
+        {
+            double? resultat = 0;
+            switch (operateur)
+            {
+                case "+":
+                    resultat = nombre1 + nombre2;
+                    break;
+                case "-":
+                    resultat = nombre1 - nombre2;
+                    break;
+                case "*":
+                case "x":
+                case "X":
+                    resultat = nombre1 * nombre2;
+                    break;
+                case "/":
+                    resultat = nombre1 / nombre2;
+                    break;
+                default:
+                    resultat = null;
+                    break;
+            }
+
+            return resultat;
+            //Tout le code après le return n'est jamais exécuté
         }
     }
 }
