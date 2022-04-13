6dc821f6061f038b19fab2b8094a2f4e355e0a0a
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-04-13 18:58:54 +0200

    ajout des méthodes surchrgées

---
+++ MethodeSurchargee/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodeSurchargee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+                double prix = Convert.ToDouble(Console.ReadLine());
+                int remise = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(reducPrice(prix, remise));
+            }
+
+
+        }
+
+        static string ConcatWithSpace(string debut, string fin)
+        {
+            return debut + " " + fin;
+        }
+
+        static double reducPrice(double price, int reduc)
+        {
+            double remise = price * ((double)reduc/100);
+            double finnalyPrice = price - remise;
+            return finnalyPrice;
+        }
+    }
+}
