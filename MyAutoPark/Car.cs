ed91928542b10f80ba66746688e649ff7b27f31e
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-04-18 12:34:06 +0200

    Ajout du projet myAutoPark

---
+++ MyAutoPark/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAutoPark
+{
+    public class Car
+    {
+        public string Couleur;
+        public string Marque;
+        public string Modele;
+        public int Puissance;
+        public int TailleRoues;
+        public int NbPortes;
+        public string Carburant;
+        public int Kilometrage;
+
+        /// <summary>
+        /// Ajoute le nombre de kilomètres mentionnés à la propriété Kilometrage
+        /// </summary>
+        /// <param name="nbKilometres">Nombre de kilomètres à ajouter</param>
+        public void Rouler(int nbKilometres)
+        {
+            Kilometrage += nbKilometres;
+        }
+    }
+}
---
+++ MyAutoPark/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyAutoPark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Créer notre objet en instanciant notre classe
+            Car PeugeotRczDeDominique = new Car();
+            //On va donner les propriétés de notre objet
+            PeugeotRczDeDominique.Couleur = "Gris";
+            PeugeotRczDeDominique.Marque = "Peugeot";
+            PeugeotRczDeDominique.Modele = "RCZ";
+            PeugeotRczDeDominique.Puissance = 156;
+            PeugeotRczDeDominique.TailleRoues = 17;
+            PeugeotRczDeDominique.NbPortes = 3;
+            PeugeotRczDeDominique.Carburant = "Gazoil";
+            PeugeotRczDeDominique.Kilometrage = 150000;
+            
+            //On utilise notre objet
+            PeugeotRczDeDominique.Rouler(15);
+            Console.WriteLine(PeugeotRczDeDominique.Marque + " " + PeugeotRczDeDominique.Modele);
+
+
+            Console.ReadLine();
+        }
+    }
+}
