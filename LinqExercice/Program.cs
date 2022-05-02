f05fe2e9c3d6c5f2d720ede52588df1157e60f7e
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-05-02 12:32:31 +0200

    Ajout d'une rquete linq

--- LinqExercice/Program.cs
+++ LinqExercice/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DataSources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqExercice
 {
@@ -7,6 +10,30 @@ namespace LinqExercice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            List<Album> maListe = ListAlbumsData.ListAlbums;
+
+            //Ecrire tous les éléments de ma liste qui comporte les lettres ba
+
+            //Avec Linq en syntaxe requête
+            var malisteQuery = from album in maListe
+                               where album.Title.Contains("ba")
+                               select album;
+
+
+            //Avec Linq en syntaxe méthode
+            var malisteMethode = maListe.Where(album => album.Title.Contains("ba"));
+
+            //Sans linq, avec une boucle
+            foreach(var album in maListe)
+            {
+                if (album.Title.Contains("ba"))
+                {
+                    Console.WriteLine(album.Title);
+                }
+            }
+
+
         }
     }
 }
